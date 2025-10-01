a4e4f190e57b222673f6c76802771bcfdc30c852
sbv0815 <115514271+sbv0815@users.noreply.github.com>
2025-10-01 08:41:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -507,8 +507,9 @@ async def get_factura_image(factura_id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
 
 
 
